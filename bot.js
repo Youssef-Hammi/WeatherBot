c940b080c997de096e9fd38afac8258f7fbed90a
Yawsef <109879943+Youssef-Hammi@users.noreply.github.com>
2024-10-14 22:53:44 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,43 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const axios = require('axios');
+
+const token = 'MTE3MjU4MzUxOTgzMTIwODAyOA.Ggy8J8.-poRTf24S6X8lCe2btP5qIldlRl-jN0fE1jiNA';
+
+const weatherstackApiKey = '2b4d78b9a41d7c8c8863df817c5aa763';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log('Weather Bot is online!');
+});
+client.on('messageCreate', async message => {
+    if (message.author.bot) return;
+    if (message.content.startsWith('!weather')) {
+        const cityName = message.content.split(' ').slice(1).join(' ');
+        if (!cityName) {
+            message.reply('Please provide a city name.');
+            return;
+        }
+
+        // Build the Weatherstack API URL
+        const weatherUrl = `http://api.weatherstack.com/current?access_key=${weatherstackApiKey}&query=${encodeURIComponent(cityName)}`;
+
+        try {
+            const response = await axios.get(weatherUrl);
+            const data = response.data;
+            if (data.error) {
+                message.reply(data.error.info);
+                return;
+            }
+            const temp = data.current.temperature;
+            const weatherDescription = data.current.weather_descriptions[0];
+            const city = data.location.name;
+            const country = data.location.country;
+            message.reply(`The weather in ${city}, ${country} is ${temp}°C with ${weatherDescription}.`);
+        } catch (error) {
+            console.error(error);
+            message.reply('There was an error retrieving the weather data.');
+        }
+    }
+});
+client.login(token);
